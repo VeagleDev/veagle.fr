5e91513719825b1ff9a5a3318b7421fdba11e92a
pierrbt <pierre08bt@gmx.co.uk>
2023-10-25 02:22:58 +0200

    fix: next config

--- next.config.js
+++ next.config.js
@@ -9,6 +9,39 @@ const nextConfig = {
         ]
     },
 
+    reactStrictMode: true,
+    swcMinify: true,
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'DENY',
+                    },
+                    {
+                        key: 'Content-Security-Policy',
+                        value:
+                            "default-src 'self' 'https://veagle.fr';",
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+                    },
+                    {
+                        key: 'Permissions-Policy',
+                        value: "camera=(); battery=(); geolocation=(); microphone=()",
+                    },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'origin-when-cross-origin',
+                    },
+                ],
+            },
+        ];
+    },
+
 }
 
 module.exports = nextConfig
