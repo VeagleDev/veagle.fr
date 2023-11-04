9f923ad74955f54085b7be873c0bb2a9b9c9d96f
pierrbt <pierre08bt@gmx.co.uk>
2023-11-05 00:07:14 +0100

    refactor: format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,24 @@ import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import PlausibleProvider from "next-plausible";
 import "./globals.scss";
-import {Author} from "next/dist/lib/metadata/types/metadata-types";
+import { Author } from "next/dist/lib/metadata/types/metadata-types";
 
 const manrope = Manrope({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Veagle | Organisation de développement informatique",
-  description: "Bienvenue sur la page d'accueil de Veagle. Vous trouverez ici toutes les informations nécessaires pour nous contacter et découvrir nos projets informatiques, ainsi que nos services.",
+  description:
+    "Bienvenue sur la page d'accueil de Veagle. Vous trouverez ici toutes les informations nécessaires pour nous contacter et découvrir nos projets informatiques, ainsi que nos services.",
   applicationName: "Veagle",
-  authors: [{ name: "Veagle", url: "https://github.com/VeagleDev"}],
-  keywords: ["development", "computer", "organisation", "tech", "programming", "web"],
+  authors: [{ name: "Veagle", url: "https://github.com/VeagleDev" }],
+  keywords: [
+    "development",
+    "computer",
+    "organisation",
+    "tech",
+    "programming",
+    "web",
+  ],
   creator: "Veagle",
   robots: "index, follow",
   manifest: "/manifest.json",
@@ -26,9 +34,13 @@ export default function RootLayout({
     <html lang="fr">
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <script defer data-domain="veagle.fr" src="https://acabou.veagle.fr/js/script.js"></script>
+        <script
+          defer
+          data-domain="veagle.fr"
+          src="https://acabou.veagle.fr/js/script.js"
+        ></script>
       </head>
-        <body className={manrope.className}>{children}</body>
+      <body className={manrope.className}>{children}</body>
     </html>
   );
 }
